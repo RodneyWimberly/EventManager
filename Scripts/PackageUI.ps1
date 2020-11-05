4adac04af44e530bf0dd5307245ae99ade73736b
Rodney Wimberly <rodney.wimberly@gmail.com>
2020-11-05 02:27:21 -0800

    Updated scripts to use dynamic folder

--- Scripts/PackageUI.ps1
+++ Scripts/PackageUI.ps1
@@ -1,7 +1,10 @@
 ﻿#
 # PackageUI.ps1
 #
-cls
-cd c:\Projects\EventManager\Web\ClientApp
+#   This script is used to build the Angular SPA and prepare it for deployment
+Clear-Host
+$location = Get-Location
+Set-Location "..\Web\ClientApp"
 npm install
 npm run build
+Set-Location $location
--- Scripts/RunUI.ps1
+++ Scripts/RunUI.ps1
@@ -1,6 +1,9 @@
 #
 # RunUI.ps1
 #
-cls
-cd c:\Projects\EventManager\Web\ClientApp
+#   This script is used to build the Angular SPA and then host it in NodeJS
+Clear-Host
+$location = Get-Location
+Set-Location "..\Web\ClientApp"
 ng serve
+Set-Location $location
